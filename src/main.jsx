4f3db93c7fd8e138d4c3b03e26330c21e9548cdb
iamernesto14 <eernesto211@gmail.com>
2025-02-03 01:51:17 +0000

    added basename

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter as Router, Link } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Router>
+    <Router basename="/CodeCompass">
       <App />
     </Router>
   </StrictMode>
